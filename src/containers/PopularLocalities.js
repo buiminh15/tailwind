086370d63f8c16970d5411b204722d7958c8b3ab
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2022-06-08 23:11:34 +0700

    Revert "feat: test revert"
    This reverts commit 26b80bb614403756156714a7dd0a23e33314d18a.

--- src/containers/PopularLocalities.js
+++ src/containers/PopularLocalities.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import PopularLocation from '../components/PopularLocation'
-import { SeeMore, SeeLess } from '../components/PopularLocation/SeeMore'
-// test
-function PopularLocalities() {
-  const [showMore, setShowMore] = useState(false)
-  const [showLess, setShowLess] = useState(false)
-  const [slicedArr, setSlicedArr] = useState()
+import React, { useEffect, useState } from "react";
+import PopularLocation from "../components/PopularLocation";
+import { SeeMore, SeeLess } from "../components/PopularLocation/SeeMore";
 
-  const arr = Array(4).fill(1)
-  const len = arr.length
+function PopularLocalities() {
+  const [showMore, setShowMore] = useState(false);
+  const [showLess, setShowLess] = useState(false);
+  const [slicedArr, setSlicedArr] = useState();
+
+  const arr = Array(4).fill(1);
+  const len = arr.length;
 
   useEffect(() => {
-    handleArr()
-  }, [])
+    handleArr();
+  }, []);
 
   const handleArr = () => {
     if (len >= 9) {
-      setSlicedArr(arr.slice(0, 8))
-      setShowMore(true)
+      setSlicedArr(arr.slice(0, 8));
+      setShowMore(true);
     } else if (len >= 6) {
-      setSlicedArr(arr.slice(0, 5))
-      setShowMore(true)
+      setSlicedArr(arr.slice(0, 5));
+      setShowMore(true);
     } else if (len >= 3) {
-      setSlicedArr(arr.slice(0, 2))
-      setShowMore(true)
+      setSlicedArr(arr.slice(0, 2));
+      setShowMore(true);
     } else {
-      setSlicedArr(arr)
+      setSlicedArr(arr);
     }
-  }
+  };
 
   const handleSeeMore = () => {
-    setShowMore(false)
-    setSlicedArr(arr)
-    setShowLess(true)
-  }
+    setShowMore(false);
+    setSlicedArr(arr);
+    setShowLess(true);
+  };
   const handleSeeLess = () => {
-    setShowLess(false)
-    handleArr()
-  }
+    setShowLess(false);
+    handleArr();
+  };
 
   return (
-    <div className='my-20 text-5xl'>
-      Popular localities in and around{' '}
-      <span className='font-semibold'>Delhi NCR</span>
-      <div className='mt-10 grid grid-cols-3 gap-4'>
+    <div className="my-20 text-5xl">
+      Popular localities in and around{" "}
+      <span className="font-semibold">Delhi NCR</span>
+      <div className="mt-10 grid grid-cols-3 gap-4">
         {slicedArr?.map((i, idx) => (
           <PopularLocation key={`popular-location-${idx}`} />
         ))}
@@ -51,7 +51,7 @@ function PopularLocalities() {
         {showLess && <SeeLess handleSeeLess={handleSeeLess} />}
       </div>
     </div>
-  )
+  );
 }
 
-export default PopularLocalities
+export default PopularLocalities;
